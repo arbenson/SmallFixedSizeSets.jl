7ed5f39b57603b10aff725764aa2df6dba92da98
Austin Benson <arbenson@gmail.com>
2018-02-14 20:15:51 -0500

    simplify things

--- src/SmallFixedSizeSets.jl
+++ src/SmallFixedSizeSets.jl
@@ -2,7 +2,7 @@ module SmallFixedSizeSets
 
 export Set0, Set1, Set2, Set3, Set4, Set5, SmallFixedSizeSet
 export SFSSFromOrderedVec, SFSSFromVec, SFSSFromSet
-export SFSS_subset, SFSS_intersect, SFSS_setdiff, SFSS_union
+export SFSS_issubset, SFSS_intersect, SFSS_setdiff, SFSS_union
 
 const Set0 = NTuple{0,Int32}
 const Set1 = NTuple{1,Int32}
@@ -44,121 +44,18 @@ SFSSFromSet(S::Set{Int32}) =
     SFSSFromOrderedVec(sort(collect(S)))
 
 # Assumption: sets are sorted
-
-# First set is size 0
-SFSS_subset(A::Set0, B::Set0) = true
-SFSS_subset(A::Set0, B::Set1) = true
-SFSS_subset(A::Set0, B::Set2) = true
-SFSS_subset(A::Set0, B::Set3) = true
-SFSS_subset(A::Set0, B::Set4) = true
-SFSS_subset(A::Set0, B::Set5) = true
-
-# First set is size 1
-SFSS_subset(A::Set1, B::Set0) = false
-SFSS_subset(A::Set1, B::Set1) = (A == B)
-function SFSS_subset(A::Set1, B::Set2)
-    a1 = A[1]
-    return a1 == B[1] || a1 == B[2]
-end
-function SFSS_subset(A::Set1, B::Set3)
-    a1 = A[1]
-    return a1 == B[1] || a1 == B[2] || a1 == B[3]
-end
-function SFSS_subset(A::Set1, B::Set4)
-    a1 = A[1]
-    return a1 == B[1] || a1 == B[2] || a1 == B[3] || a1 == B[4]
-end
-function SFSS_subset(A::Set1, B::Set5)
-    a1 = A[1]; return a1 == B[1] || a1 == B[2] || a1 == B[3] || a1 == B[4] || a1 == B[5]
-end
-
-# First set is size 2
-SFSS_subset(A::Set2, B::Set0) = false
-SFSS_subset(A::Set2, B::Set1) = false
-SFSS_subset(A::Set2, B::Set2) = (A == B)
-function SFSS_subset(A::Set2, B::Set3)
-    a1, a2 = A
-    b1, b2, b3 = B
-    if (a1 == b1) && (a2 == b2 || a2 == B[3]); return true; end
-    if (a1 == b2) && (a2 == B[3]); return true; end
-    return false
-end
-function SFSS_subset(A::Set2, B::Set4)
-    a1, a2 = A
-    b1, b2, b3, b4 = B
-    if (a1 == b1) && (a2 == b2 || a2 == b3 || a2 == b4); return true; end
-    if (a1 == b2) && (a2 == b3 || a2 == b4); return true; end
-    if (a1 == b3) && (a2 == b4); return true; end
-    return false
-end
-function SFSS_subset(A::Set2, B::Set5)
-    a1, a2 = A
-    b1, b2, b3, b4, b5 = B
-    if (a1 == b1) && (a2 == b2 || a2 == b3 || a2 == b4 || a2 == b5); return true; end
-    if (a1 == b2) && (a2 == b3 || a2 == b4 || a2 == b5); return true; end
-    if (a1 == b3) && (a2 == b4 || a2 == b5); return true; end
-    if (a1 == b4) && (a2 == b5); return true; end
-    return false
-end
-
-# First set is size 3
-SFSS_subset(A::Set3, B::Set0) = false
-SFSS_subset(A::Set3, B::Set1) = false
-SFSS_subset(A::Set3, B::Set2) = false
-SFSS_subset(A::Set3, B::Set3) = (A == B)
-function SFSS_subset(A::Set3, B::Set4)
-    a1, a2, a3 = A
-    b1, b2, b3, b4 = B
-    if (a1 == b1)
-        if (a2 == b2) && (a3 == b3 || a3 == b4); return true; end
-        if (a2 == b3) && (a3 == b4); return true; end
-    end
-    if (a1 == b2) && (a2 == b3) && (a3 == b4); return true; end
-    return false
-end
-function SFSS_subset(A::Set3, B::Set5)
-    a1, a2, a3 = A
-    b1, b2, b3, b4, b5 = B
-    if (a1 == b1)
-        if (a2 == b2) && (a3 == b3 || a3 == b4 || a3 == b5); return true; end
-        if (a2 == b3) && (a3 == b4 || a3 == b5); return true; end
-        if (a2 == b4) && (a3 == b5); return true; end
-    end
-    if (a1 == b2)
-        if (a2 == b3) && (a3 == b4 || a3 == b5); return true; end
-        if (a2 == b4) && (a3 == b5); return true; end
-    end
-    if (a1 == b3) && (a2 == b4) && (a3 == b5); return true; end
-    return false
-end
-
-# First set is size 4
-SFSS_subset(A::Set4, B::Set0) = false
-SFSS_subset(A::Set4, B::Set1) = false
-SFSS_subset(A::Set4, B::Set2) = false
-SFSS_subset(A::Set4, B::Set3) = false
-SFSS_subset(A::Set4, B::Set4) = (A == B)
-function SFSS_subset(A::Set4, B::Set5)
-    a1, a2, a3, a4 = A
-    b1, b2, b3, b4, b5 = B
-    if (a1 == b1)
-        if (a2 == b2)
-            if (a3 == b3) && (a4 == b4 || a4 == b5); return true; end
-            if (a3 == b4 && a4 == b5); return true; end
+function SFSS_issubset(A::SmallFixedSizeSet, B::SmallFixedSizeSet)
+    nA, nB = length(A), length(B)
+    Aind, Bind = 1, 1
+    while Aind <= nA && Bind <= nB
+        aval, bval = A[Aind], B[Bind]        
+        if   aval < bval;  return false
+        else Bind += 1;
         end
-        if (a2 == b3 && a3 == b4 && a4 == b5); return true; end
+        Aind += (aval == bval)
     end
-    if (a1 == b2 && a2 == b3 && a3 == b4 && a4 == b5); return true; end
-    return false
+    return Aind > nA
 end
-
-# First set is size 5
-SFSS_subset(A::Set5, B::Set0) = false
-SFSS_subset(A::Set5, B::Set1) = false
-SFSS_subset(A::Set5, B::Set2) = false
-SFSS_subset(A::Set5, B::Set3) = false
-SFSS_subset(A::Set5, B::Set4) = false
-SFSS_subset(A::Set5, B::Set5) = (A == B)
 
 """
 SFSS_intersect
